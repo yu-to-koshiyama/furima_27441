4c950fa4013fca7e791bbdda0c91f141aab9e44c
Yuto Koshiyama <yu_tok54@icloud.com>
2020-07-23 19:16:21 +0900

    make-Factory_bot

---
+++ spec/factories/users.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+
+  factory :user do
+    nickname              {"furimaくん"}
+    email                 {"sample@gmail.com"}
+    password              {"test0000"}
+    password_confirmation {"test0000"}
+    family_name           {"山田"}
+    first_name            {"陸太郎"}
+    family_name_kana      {"ヤマダ"}
+    first_name_kana       {"リクタロウ"}
+    birthday              {"1930-01-01"}
+  end
+end
