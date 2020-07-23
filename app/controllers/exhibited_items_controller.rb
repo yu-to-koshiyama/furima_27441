798aea315824df23dfafc9ddff9e6e135455dd9c
Yuto Koshiyama <yu_tok54@icloud.com>
2020-07-23 16:09:53 +0900

    rename-items-view

--- app/controllers/exhibited_items_controller.rb
+++ app/controllers/exhibited_items_controller.rb
@@ -1,7 +1,12 @@
 class ExhibitedItemsController < ApplicationController
+  before_action :move_to_index, except: [:index]
+  def index
+  end
 
-def index
-end
-
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
 
 end
