b5c7b02cadec62c602a60ead15810e68637eb74b
Yuto Koshiyama <yu_tok54@icloud.com>
2020-07-23 15:52:18 +0900

    migration-Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,12 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+        :recoverable, :rememberable, :validatable
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+  validates :encrypted_password, presence: true, format:{ with: /[a-z\d]{6,}/i }
+
+  validates :family_name, :first_name, presence: true, format:{ with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :family_name_kana, :first_name_kana, presence: true, format:{with: /\A[ァ-ヶー－]+\z/}
 end
