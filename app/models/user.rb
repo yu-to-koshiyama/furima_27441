e560fc52004b271d7a1fe85a44c3aa915f60544d
Yuto Koshiyama <yu_tok54@icloud.com>
2020-07-24 16:29:53 +0900

    edit-validation-User

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   validates :nickname, presence: true      
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
-  validates :encrypted_password, presence: true, format:{ with: /[a-z\d]{6,}/i }
+  validates :password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]{6,}+\z/i }
 
   validates :family_name, :first_name, presence: true, format:{ with: /\A[ぁ-んァ-ン一-龥]/ }
   validates :family_name_kana, :first_name_kana, presence: true, format:{with: /\A[ァ-ヶー－]+\z/}
