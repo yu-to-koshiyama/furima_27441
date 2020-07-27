1a27f44f2d2b2e5a8d41fbda4c70c3c51789aff0
Yuto Koshiyama <yu_tok54@icloud.com>
2020-07-27 12:19:32 +0900

    make-link-to-newExhibited-item

--- app/controllers/exhibited_items_controller.rb
+++ app/controllers/exhibited_items_controller.rb
@@ -1,8 +1,13 @@
 class ExhibitedItemsController < ApplicationController
-  before_action :move_to_index, except: [:index]
+  before_action :move_to_index, except: [:index, :new]
   def index
   end
 
+  def new
+  
+  end
+  
+  private
   def move_to_index
     redirect_to action: :index unless user_signed_in?
   end
