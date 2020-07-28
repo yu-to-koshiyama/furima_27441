79354dea243ccf209ef4aab6e0d5df7f986f3c2f
Yuto Koshiyama <yu_tok54@icloud.com>
2020-07-28 14:08:29 +0900

    create-new-action

--- app/controllers/exhibited_items_controller.rb
+++ app/controllers/exhibited_items_controller.rb
@@ -7,9 +7,22 @@ class ExhibitedItemsController < ApplicationController
   def new
     @exhibitedItem = ExhibitedItem.new
   end
+
+  def create
+    @exhibitedItem = ExhibitedItem.new(exhibitedItem_params)
+    if @exhibitedItem.save
+      redirect_to root_path
+    else
+      render "new"
+    end
+  end
   
   private
   def move_to_index
     redirect_to action: :index unless user_signed_in?
+  end
+
+  def exhibitedItem_params
+    params.require(:exhibited_item).permit(:name, :explanation, :price, :category_id, :status_id, :payment_id, :prefecture_id, :until_delivery_id, :image).merge(user_id: current_user.id)
   end
 end
