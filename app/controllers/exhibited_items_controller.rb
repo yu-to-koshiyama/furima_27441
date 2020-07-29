469ad374e0a1e1cb9476c7154d313621f8257872
Yuto Koshiyama <yu_tok54@icloud.com>
2020-07-29 20:49:40 +0900

    edit-dealing-to-index

--- app/controllers/exhibited_items_controller.rb
+++ app/controllers/exhibited_items_controller.rb
@@ -2,6 +2,7 @@ class ExhibitedItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
   def index
     @exhibitedItems = ExhibitedItem.all
+    @dealings = Dealing.all
   end
 
   def new
@@ -21,5 +22,5 @@ class ExhibitedItemsController < ApplicationController
   def exhibitedItem_params
     params.require(:exhibited_item).permit(:name, :explanation, :price, :category_id, :status_id, :payment_id, :prefecture_id, :until_delivery_id, :image).merge(user_id: current_user.id)
   end
-  
+
 end
