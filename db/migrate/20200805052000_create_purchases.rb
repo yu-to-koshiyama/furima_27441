57684676f5b703ca414f68966fabac0302df0f02
Yuto Koshiyama <yu_tok54@icloud.com>
2020-08-05 14:22:16 +0900

    make-purchase-table

--- db/migrate/20200805052000_create_purchases.rb
+++ db/migrate/20200805052000_create_purchases.rb
@@ -1,6 +1,13 @@
 class CreatePurchases < ActiveRecord::Migration[6.0]
   def change
     create_table :purchases do |t|
+      t.string     :postal_code,        null: false
+      t.integer    :prefecture_id,      null: false
+      t.string     :city,               null: false
+      t.string     :house_number,       null: false
+      t.string     :building,           default: "" 
+      t.string     :phone_number,       null: false
+      t.references :exhibited_item,     foreign_key: true
 
       t.timestamps
     end
