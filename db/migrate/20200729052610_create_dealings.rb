a6a5558db210a14c348f196e39abff744544ba07
Yuto Koshiyama <yu_tok54@icloud.com>
2020-08-05 14:18:53 +0900

    edit-dealing-table

--- db/migrate/20200729052610_create_dealings.rb
+++ db/migrate/20200729052610_create_dealings.rb
@@ -1,7 +1,7 @@
 class CreateDealings < ActiveRecord::Migration[6.0]
   def change
     create_table :dealings do |t|
-      t.references :user,                 foreign_key: true, null: true
+      t.references :user,                 foreign_key: true
       t.references :exhibited_item,       foreign_key: true
       t.timestamps
     end
