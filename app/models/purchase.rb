a1887ab82e9fb41d4090faf5387dcf3cb92663d7
Yuto Koshiyama <yu_tok54@icloud.com>
2020-08-05 14:33:34 +0900

    edit-purchase-model

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,2 +1,7 @@
 class Purchase < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
+
+  belongs_to :exhibited_item ,optional: true
+  has_one :dealing
 end
