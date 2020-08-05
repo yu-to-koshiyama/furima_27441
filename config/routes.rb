2fb57ee6ea71ae82cfa46a1dd3181fe952f6c1aa
Yuto Koshiyama <yu_tok54@icloud.com>
2020-08-05 14:34:44 +0900

    edit-routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'exhibited_items#index'
-  resources :exhibited_items
+  resources :exhibited_items do
+    resources :purchases, only: [:index,:new,:create]
+  end
 end
